5d8490fab92c17f6d4b4a0e32cc540ff7601f30a
Jefté Goes <jefte.goes.dev@outlook.com>
2023-08-06 21:02:18 -0300

    Adding new examples in C#.

---
+++ Example/Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+const string url = "amqp://guest:guest@localhost:5672";
+const string queueName = "books-queue";
+
+var connectionFactory = new ConnectionFactory()
+{
+    Uri = new Uri(url)
+};
+
+var connection = connectionFactory.CreateConnection();
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    Console.WriteLine("Waiting for messages...");
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"Message received: {message}");
+    };
+    channel.BasicConsume(queue: queueName,
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit, but wait for listen...");
+    Console.ReadLine();
+}
---
+++ Example/Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+const string url = "amqp://guest:guest@localhost:5672";
+const string queueName = "books-queue";
+
+var connectionFactory = new ConnectionFactory()
+{
+    Uri = new Uri(url)
+};
+
+var connection = connectionFactory.CreateConnection();
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queueName, false, false, false, null);
+
+    var book = new { Name = "Capitães da Areia" };
+    var bookSerialized = JsonSerializer.Serialize(book);
+    var bytes = Encoding.UTF8.GetBytes(bookSerialized);
+
+    channel.BasicPublish(string.Empty, queueName, false, null, bytes);
+}
